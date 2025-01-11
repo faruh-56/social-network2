bfcdde79426ff598c59941918c6c56c97ba23f02
faruh-26 <valievfarruh6@gmail.com>
2025-01-11 13:03:08 +0500

    dz 41

---
+++ src/components/Post/postData.tsx
@@ -0,0 +1,61 @@
+export const postData = [
+    { 
+      id: 0,
+      picture: (
+        <img
+          className="media__item"
+          src="./img/post/nature-1.png"
+          alt="Post Item"
+        />
+      )
+    },
+    { id: 1,
+      picture: (
+        <img
+          className="media__item"
+          src="./img/post/nature-2.png"
+          alt="Post Item"
+        />
+      )
+    },
+    { 
+      id: 2,
+      picture: (
+        <img
+          className="media__item"
+          src="./img/post/nature-3.png"
+          alt="Post Item"
+        />
+      )
+    },
+    { 
+      id: 3,
+      picture: (
+        <img
+          className="media__item"
+          src="./img/post/nature-4.png"
+          alt="Post Item"
+        />
+      )
+    },
+    { 
+      id: 4,
+      picture: (
+        <img
+          className="media__item"
+          src="./img/post/nature-5.png"
+          alt="Post Item"
+        />
+      )
+    },
+    { 
+      id: 5,
+      picture: (
+        <img
+          className="media__item"
+          src="./img/post/nature-6.png"
+          alt="Post Item"
+        />
+      )
+    }
+  ]
